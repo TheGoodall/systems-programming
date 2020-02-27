12714556e1836b6b070cbfd107cf80260cc48c66
James Goodall <james@frizlette.me>
2020-02-27 13:59:33 +0000

    done a load more work

--- gol.c
+++ gol.c
@@ -1,16 +1,21 @@
+#include<stdlib.h>
 #include<stdio.h>
+#include<string.h>
 #include<stdbool.h>
 #include"gol.h"
 
 void read_in_file(FILE *infile, struct universe *u){
-    u->grid = malloc(1);
-	int lines = 1;
-	int lengthOfFirstLine = 0;
-	char charBuffer;
-	while (
-
+    u->width = -1;
+    u->height = 0;
+    u->grid = malloc(u->height * sizeof(int*));
+    char line[512] = "";
+    while (fgets(line, 512, infile) != NULL){
+        u->height++;
+        u->grid = realloc(u->grid, u->height * sizeof(int));
+        u->grid[u->height-1] = line;
+        }
+    }
 }
-
 void write_out_file(FILE *outfile, struct universe *u){
 
 }
